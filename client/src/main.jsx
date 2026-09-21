a6f5281b8f04b57cee9ebed4ccdf15007fabe870
chunkyoungmin <chunkyoungmin0818@gmail.com>
2026-09-22 02:43:26 +0900

    codex 수정

---
+++ client/src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import axios from 'axios'
+import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
+import './index.css'
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || ''
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
